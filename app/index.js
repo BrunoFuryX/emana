c92e95a17f4969b0a20460b280de4b9c4e3f6e15
Bruno Lopes <bruno.lopes@auaha.com.br>
2021-11-22 18:59:13 -0300

    initial

--- app/index.js
+++ app/index.js
@@ -1,4 +1,3 @@
-// import "./monitoring.js"
 import React from "react"
 import ReactDOM from "react-dom"
 
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var { watch, src, dest, parallel, series } = require("gulp"),
     rename = require("gulp-rename"),
     concat = require("gulp-concat"),
     cleanCSS = require("gulp-clean-css")
+    uglify = require("gulp-uglify")
+
 
 function css() {
     return src("src/**/*.styl")
@@ -23,20 +25,20 @@ function css() {
         .pipe(dest("opencode/css"))
 }
 
+
 function js() {
     return src("src/**/*.js", { sourcemaps: false })
         .pipe(jsImport({ hideConsole: true }))
-        .pipe(concat("app.js"))
-        .pipe(
-            minify({
-                ext: {
-                    src: ".js",
-                    min: ".min.js"
-                },
-                exclude: ["tasks"],
-                ignoreFiles: [".combo.js", "-min.js"]
-            })
-        )
+        .pipe(minify({
+            ext: {
+                src: ".js",
+                min: ".min.js"
+            },
+            exclude: ["tasks"],
+            ignoreFiles: [".combo.js", "-min.js"]
+        }))
+        .pipe(concat("app.min.js"))
+        .pipe(uglify({ mangle: false }))
         .pipe(dest("opencode/js", { sourcemaps: false }))
 }
 
